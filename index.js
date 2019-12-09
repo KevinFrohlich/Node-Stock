301cd30adacb0a074a530a75cba6a380fc06af01
Kevin Frohlich <kevinfrohlich@gmail.com>
2019-12-09 15:42:36 -0800

    setup Handlebars

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+var exphbs = require("express-handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// view engine setup
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server listening on Port: " + PORT));
