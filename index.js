cdbc1c9fbf4b55eef57f420c276c28a8cfa17388
Kevin Frohlich <kevinfrohlich@gmail.com>
2020-02-09 11:50:39 -0800

    created Procfile for Heroku.

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const app = express();
 const path = require("path");
 const exphbs = require("express-handlebars");
 const request = require("request");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
+
+// Use body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // API Token: pk_e95c28451060417dba76769b63faf5af
 // Create call API function
-function callApi(finishedApi) {
+function callApi(finishedApi, ticker) {
   request(
-    "https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_e95c28451060417dba76769b63faf5af",
+    "https://cloud.iexapis.com/stable/stock/" +
+      ticker +
+      "/quote?token=pk_e95c28451060417dba76769b63faf5af",
     { json: true },
     (err, res, body) => {
       if (err) {
@@ -26,13 +33,23 @@ function callApi(finishedApi) {
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-// Set handlebar routes
+// Set handlebar index GET route
 app.get("/", function(req, res) {
   callApi(function(doneApi) {
     res.render("home", {
       stock: doneApi
     });
-  });
+  }, "fb");
+});
+
+// Set handlebar index POST route
+app.post("/", function(req, res) {
+  callApi(function(doneApi) {
+    //posted_info = req.body.stockTicker;
+    res.render("home", {
+      stock: doneApi
+    });
+  }, req.body.stockTicker);
 });
 
 app.get("/about.html", function(req, res) {
